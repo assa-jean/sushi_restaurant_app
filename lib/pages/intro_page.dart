e781132ff575f4665992a963e2150a67a3b8f002
assa-jean <assa19@yahoo.com>
2024-01-24 03:23:12 +0100

    Added some features for Menu page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -6,7 +7,33 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            //shop name
+            Text(
+              "Sushi Boy",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //icon
+
+            //title
+
+            //subtitle
+
+            //get started button
+          ],
+        ),
+      ),
     );
   }
 }
