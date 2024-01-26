7311434bbeeb1e56560e678fa1c48a205ce7b991
assa-jean <assa19@yahoo.com>
2024-01-26 01:15:28 +0100

    Added the title, star and description

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../models/food.dart';
 
 class FoodDetailsPage extends StatefulWidget {
-  const FoodDetailsPage({super.key});
+  final Food food;
+  const FoodDetailsPage({super.key, required this.food});
 
   @override
   State<FoodDetailsPage> createState() => _FoodDetailsPageState();
@@ -10,6 +14,80 @@ class FoodDetailsPage extends StatefulWidget {
 class _FoodDetailsPageState extends State<FoodDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 81, 45, 45),
+      ),
+      body: Column(
+        children: [
+          //listview of food details
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ListView(
+                children: [
+                  //image
+                  Image.asset(
+                    widget.food.imagePath,
+                    height: 200,
+                  ),
+
+                  const SizedBox(height: 25),
+                  //rating
+                  Row(
+                    children: [
+                      //star icon
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow[700],
+                      ),
+                      const SizedBox(width: 5),
+                      //raing
+                      Text(
+                        widget.food.rating,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  //food name
+                  Text(
+                    widget.food.name,
+                    style: GoogleFonts.dmSerifDisplay(fontSize: 30),
+                  ),
+
+                  const SizedBox(height: 25),
+                  //Food description
+                  Text(
+                    "Description",
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Delicate sliced, fresh salmon drapes elegantly over a pillow of perfectly seasoned sushi rice.Delicate sliced, fresh salmon drapes elegantly over a pillow of perfectly seasoned sushi rice.Delicate sliced, fresh salmon drapes elegantly over a pillow of perfectly seasoned sushi rice.",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14,
+                      height: 2,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          //price + qte + add to cart button
+        ],
+      ),
+    );
   }
 }
